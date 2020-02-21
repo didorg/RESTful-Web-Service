dab0e40afa92e5aa1c6c549255de4305eccaef6a
Didier <didorg@gmail.com>
2020-02-21 13:19:38 -0500

    update GreetingController

--- src/main/java/com/dido/mrws/controllers/GreetingController.java
+++ src/main/java/com/dido/mrws/controllers/GreetingController.java
@@ -3,16 +3,19 @@ package com.dido.mrws.controllers;
 import com.dido.mrws.constans.Params;
 import com.dido.mrws.model.Person;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/person")
 public class GreetingController {
 
   private final AtomicLong id = new AtomicLong();
 
-  @RequestMapping("/person")
+  @GetMapping
   public Person greeting(
       @RequestParam(value = "name", defaultValue = "enter param -> ?name=value") String name,
       @RequestParam(value = "lastName", defaultValue = "enter param -> ?lastName=value") String lastName,
